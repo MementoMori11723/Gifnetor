68678a8ff710d36bc8fe838cdd149df89f29fceb
yasasvi <yasasvigumma@gmail.com>
2024-01-21 12:44:44 +0530

    re-structuring...

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask,render_template
+import ffmpeg
+
+def convert(filename,output):
+    stream = ffmpeg.input(filename)
+    stream = ffmpeg.output(stream,output)
+    ffmpeg.run_async(stream)
+
 app = Flask(__name__)
 @app.route("/")
 def index():
--- conv.py
+++
@@ -1,5 +0,0 @@
-import ffmpeg
-def convert(filename,output):
-    stream = ffmpeg.input(filename)
-    stream = ffmpeg.output(stream,output)
-    ffmpeg.run_async(stream)
