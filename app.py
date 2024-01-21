ca84e1b41169fa8a4ebcd4d20f8602a7205bd15f
yasasvi <yasasvigumma@gmail.com>
2024-01-21 20:13:20 +0530

    need to make a few changes

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 import ffmpeg
+import os
+
+app = Flask(__name__)
+
+def fetch(filename):
+    path = os.path.join('uploads/',filename)
+    convert(path,os.path.join('uploads/','output.gif'))
+    print("Complete")
+
 
 def convert(filename,output):
     stream = ffmpeg.input(filename)
     stream = ffmpeg.output(stream,output)
-    ffmpeg.run_async(stream)
+    ffmpeg.run(stream)
+    return
 
-app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
 @app.route("/about/")
 def about():
     return render_template("about.html")
+
+@app.route("/upload/",methods=['POST'])
+def upload():
+    file = request.files['video']
+    file.save(os.path.join('uploads/',file.filename)) if file else None
+    fetch(file.filename)
+    return redirect("/")
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask_frozen import Freezer
 from app import app
+from flask_frozen import Freezer
 freezer = Freezer(app)
 freezer.freeze()
+# app.run(debug=True)
